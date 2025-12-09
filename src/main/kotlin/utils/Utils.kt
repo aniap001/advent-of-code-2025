29661a8b069ca3f53f9175ef4060242d53b11e52
Anna <anapiezak@gmail.com>
2025-12-09 09:00:33 +0000

    Solution for the problem from day 1

---
+++ src/main/kotlin/utils/Utils.kt
@@ -0,0 +1,14 @@
+package org.example.utils
+
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = Path("src/main/resources/$name.txt").readText().trim().lines()
+
+/**
+ * The cleaner shorthand for printing output.
+ */
+fun Any?.println() = println(this)
