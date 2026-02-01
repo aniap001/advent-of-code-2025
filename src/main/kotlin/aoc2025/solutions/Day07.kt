9502cc600c6005a88ff04157457e4b5ecf21660b
Anna <anapiezak@gmail.com>
2026-02-01 10:57:54 +0200

    Make solution for day 7 part one more idiomatic

--- src/main/kotlin/aoc2025/solutions/Day07.kt
+++ src/main/kotlin/aoc2025/solutions/Day07.kt
@@ -1,28 +1,32 @@
 package aoc2025.solutions
 
+data class State(val beams: Set<Int>, val counter: Int)
+
 object Day07: Day {
-    data class State(var beams: Set<Int>, var counter: Int)
 
     override fun solvePartOne(input: List<String>): Number {
         val startingIndex = input[0].indexOfFirst { it == 'S' }
-        val state = State(mutableSetOf(startingIndex), 0)
+        val state = State(setOf(startingIndex), 0)
 
         val finalState = input.drop(1).fold(state) { currentState, line ->
-            val nextBeams = mutableSetOf<Int>()
-            var counter = currentState.counter
-            currentState.beams.forEach {
-                if (line[it] == '^') {
-                    counter++
-                    if (it < line.length-1) nextBeams.add(it+1)
-                    if (it > 0) nextBeams.add(it-1)
-                } else {
-                    nextBeams.add(it)
-                }
-            }
-            State(nextBeams, counter)
+            val splitsInThisStep = currentState.beams.count { line[it] == '^'}
+            val nextBeams = currentState.beams.flatMap { it.changeBeamPositions(line) }.toSet()
+            currentState.copy(
+                beams = nextBeams,
+                counter = currentState.counter + splitsInThisStep
+            )
         }
-
         return finalState.counter
+    }
+
+    private fun Int.changeBeamPositions(line: String): List<Int>{
+        return when(line[this]) {
+            '^' -> listOfNotNull(
+                        if (this + 1 in line.indices) this + 1 else null,
+                        if (this - 1 in line.indices) this - 1 else null
+                    )
+            else -> listOf(this)
+        }
     }
 
     override fun solvePartTwo(input: List<String>): Number {
